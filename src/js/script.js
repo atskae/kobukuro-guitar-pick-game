552252cc5ef884f99d576d7cc70acb4fe0f1cc6a
Atsuko Shimizu <atskae@gmail.com>
2021-08-01 16:33:29 -0400

    Add more useless features

--- src/js/script.js
+++ src/js/script.js
@@ -8,14 +8,33 @@ var notHaveItImage = "images/placeholder_not_have_it.png";
 // Sound effects
 // Hopefully I won't get in trouble for this...
 var questionSound = new Audio("sounds/question.mp3");
-var kobuchiHasItSound = new Audio("sounds/kobuchi_has_it.mp3")
-var kobuchiNotHasItSound = new Audio("sounds/kobuchi_not_has_it.mp3")
+var kobuchiHasItSound = new Audio("sounds/kobuchi_has_it.mp3");
+var kobuchiNotHasItSound = new Audio("sounds/kobuchi_not_has_it.mp3");
 var haHaHaSound = new Audio("sounds/hahaha.mp3");
 var sugoiSound = new Audio("sounds/sugoi.mp3");
+
+// Random sound effects
+randomSounds = {}
+randomSounds["kobuchi"] = [
+  new Audio("sounds/kobuchi_hm.mp3"),
+  new Audio("sounds/kobuchi_look.mp3"),
+  new Audio("sounds/kobuchi_sugoi.mp3"),
+];
+randomSounds["kuroda"] = [
+  new Audio("sounds/kuroda_eejyanai.mp3"),
+  new Audio("sounds/kuroda_surprise.mp3"),
+  new Audio("sounds/kuroda_uruseh.mp3"),
+];
 
 // Document elements
 kobuchiImage = document.getElementById("kobuchi-img")
 kobuchiScore = document.getElementById("kobuchi-score");
+
+function playRandom(person) {
+  sounds = randomSounds[person];
+  sound = sounds[Math.floor(Math.random() * sounds.length)];
+  sound.play();
+}
 
 function displayHaveIt() {
   console.log("displayHaveIt()");    
